f1db857af0839ed5529474cc486ea029d2648a99
LizaCodeAndCoffee <liza.kurilova@gmail.com>
2023-11-01 23:15:03 -0700

    first

---
+++ books/store/views.py
@@ -0,0 +1,13 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+
+from store.models import Book
+from store.serializers import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['price']
+
